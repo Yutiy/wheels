d63e0572a90d3482f9d7824a93980f5d4a47c90d
杨林 <494657028@qq.com>
2021-01-15 18:13:45 +0800

    add diff 文件

--- mini-react/src/utils/diff.js
+++ mini-react/src/utils/diff.js
@@ -1,3 +1,6 @@
+import handleAttrs from '../react-dom/handleAttrs';
+import { createComponent, setComponentProps } from './render';
+
 /**
  * @param {HTMLElement} dom 真实DOM
  * @param {vnode} vnode 虚拟DOM
@@ -62,5 +65,167 @@ export function diffNode(dom, vnode) {
   // 对比函数
   if (typeof vnode.tag === 'function') {
     return diffComponent(dom, vnode);
+  }
+
+  // 对比非文本节点
+  // 1. 真实DOM不存在，表示此节点是新增的
+  // 2. 新旧两个节点的类型不一样，那么就新建一个DOM元素，并将原来的子节点（如果有的话）移动到新建的DOM节点下
+  if(!dom || !isSameNodeType(dom, vnode)) {
+    out = document.createElement(vnode.tag || vnode.type);
+    if (dom) {
+      [...dom.childNodes].map(out.appendChild); // 将原来的子节点移到新节点下
+
+      if (dom.parentNode) {
+        dom.parentNode.replaceChild(out, dom); // 移除掉原来的DOM对象
+      }
+    }
+  }
+
+  if (
+    (vnode.children && vnode.children.length > 0) ||
+    (out.childNodes && out.childNodes.length > 0)
+  ) {
+    diffChildren(out, vnode.children);
+  }
+
+  diffAttributes(out, vnode);
+  return out;
+}
+
+function diffComponent(dom, vnode) {
+  let c = dom && dom._component;
+  let oldDom = dom;
+
+  // 如果组件类型没有变化，则重新set props
+  if (c && c.constructor === vnode.tag) {
+    setComponentProps(c, vnode.attrs);
+    dom = c.base;
+  } else {  // 如果组件类型变化，则移除掉原来组件，并渲染新的组件
+    if (c) {
+      unmountComponent(c);
+      oldDom = null;
+    }
+
+    c = createComponent(vnode.tag, vnode.attrs);
+    setComponentProps(c, vnode.attrs);
+    dom = c.base;
+
+    if (oldDom && dom !== oldDom) {
+      oldDom._component = null;
+      removeNode(oldDom);
+    }
+  }
+
+  return dom;
+}
+
+function diffChildren(dom, vchildren) {
+  const domChildren = dom.childNodes;
+  const children = [];  // 没有key值的真实dom集合
+  const keyed = {};     // 有key值的集合
+
+  // 将有key的节点和没有key的节点分开
+  if (domChildren.length > 0) {
+    for (let i = 0; i < domChildren.length; i++) {
+      const child = domChildren[i];
+      const key = child.key;
+      if (key) {
+        keyed[key] = child;
+      } else {
+        children.push(child);
+      }
+    }
+  }
+
+  if (vchildren && vchildren.length > 0) {
+    let min = 0;
+    let childrenLen = children.length;
+
+    for (let i = 0; i < vchildren.length; i++) {
+      const vchild = vchildren[i];
+      const key = vchild.key;
+      let child;
+
+      // 如果有key，找到对应key值的节点
+      if (key) {
+        if (keyed[key]) {
+          child = keyed[key];
+          keyed[key] = undefined;
+        }
+      } else if (min < childrenLen) { // 如果没有key，则优先找类型相同的节点
+        for (let j = min; j < childrenLen; j++) {
+          let c = children[j];
+
+          if (c && isSameNodeType(c, vchild)) {
+            child = c;
+            children[j] = undefined;
+
+            if (j === childrenLen - 1) childrenLen--;
+            if (j === min) min++;
+            break;
+          }
+        }
+      }
+
+      child = diffNode(child, vchild);
+
+      const f = domChildren[i];
+      if (child && child !== dom && child !== f) {
+        if (!f) {
+          dom.appendChild(child);
+        } else if (child === f.nextSibling) {
+          removeNode(f);
+        } else {
+          dom.insertBefore(child, f);
+        }
+      }
+    }
+  }
+}
+
+export function diffAttributes(dom, vnode) {
+  const old = {}; // 当前DOM的属性
+  const attrs = vnode.attrs; // 虚拟DOM的属性
+
+  for (let i = 0; i < dom.attributes.length; i++) {
+    const attr = dom.attributes[i];
+    old[attr.name] = attr.value;
+  }
+
+  // 如果原来的属性不在新的属性当中，则将其移除掉（属性值设为undefined）
+  for (let name in old) {
+    if (!(name in attrs)) {
+      handleAttrs(dom, name, undefined);
+    }
+  }
+
+  // 更新新的属性值
+  for (let name in attrs) {
+    if (old[name] !== attrs[name]) {
+      handleAttrs(dom, name, attrs[name]);
+    }
+  }
+}
+
+export function isSameNodeType(dom, vnode) {
+  if (typeof vnode === 'string' || typeof vnode === 'number') {
+    return dom.nodeType === 3;
+  }
+
+  if (typeof vnode.tag === 'string') {
+    return dom.nodeName.toLowerCase() === vnode.tag.toLowerCase();
+  }
+
+  return dom && dom._component && dom._component.constructor === vnode.tag;
+}
+
+export function unmountComponent(component) {
+  if (component.componentWillUnmount) component.componentWillUnmount();
+  removeNode(component.base);
+}
+
+function removeNode(dom) {
+  if (dom && dom.parentNode) {
+    dom.parentNode.removeChild(dom);
   }
 }
