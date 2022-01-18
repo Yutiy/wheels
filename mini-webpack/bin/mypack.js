be291bb663aa8d9825abd2e45a59028dc8e9a4ff
lin.yang <lin.yang@betterwood.com>
2022-01-18 14:48:23 +0800

    fix(mini-webpack): 修复文件引用错误

--- mini-webpack/bin/mypack.js
+++ mini-webpack/bin/mypack.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 const path = require('path');
-const Compiler = require('./libs/compilers.js');
+const Compiler = require('./libs/compiler.js');
 const config = require(path.resolve('webpack.config.js'));
 
 const compiler = new Compiler(config);
